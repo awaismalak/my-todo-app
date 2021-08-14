2ce50a607af8624d60aa9b8fa3ebf1fbfdbb8749
awaismalik <awais.malik.q@gmail.com>
2021-08-14 08:15:05 +0500

    Create basice structure todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  constructor(private dialog: MatDialog) {}
+  title = 'My todo App';
+  inputData = '';
+
+  data = [
+    {
+      taskTtle: 'work',
+      timing: '10 days ago',
+    },
+    {
+      taskTtle: 'cricket',
+      timing: '12 days ago',
+    },
+    {
+      taskTtle: 'hockey',
+      timing: '13 days ago',
+    },
+    {
+      taskTtle: 'football',
+      timing: '14 days ago',
+    },
+  ];
+
+  createTask() {
+    if (this.inputData.length > 1) {
+      this.data.unshift({
+        taskTtle: this.inputData,
+        timing: 'this is auto ginrated',
+      });
+      this.inputData = '';
+    }
+  }
+  deleteTask(index: any) {
+    this.data.splice(index, 1);
+  }
+
+  openDialog() {
+    this.dialog.open(EditTaskComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, EditTaskComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [EditTaskComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
